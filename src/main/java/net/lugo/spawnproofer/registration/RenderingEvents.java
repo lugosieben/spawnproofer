754bc9b7bf1090a01ed7a3970611368df4e89d36
lugosieben <62535192+lugosieben@users.noreply.github.com>
2026-01-25 14:35:45 +0100

    rem profiler

--- src/main/java/net/lugo/spawnproofer/registration/RenderingEvents.java
+++ src/main/java/net/lugo/spawnproofer/registration/RenderingEvents.java
@@ -2,19 +2,12 @@ package net.lugo.spawnproofer.registration;
 
 import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents;
 import net.lugo.spawnproofer.OverlayManager;
-import net.minecraft.util.profiling.Profiler;
-import net.minecraft.util.profiling.ProfilerFiller;
 
 public class RenderingEvents {
     public static void register() {
-        WorldRenderEvents.BEFORE_TRANSLUCENT.register(context -> {
-            final ProfilerFiller profilerFiller = Profiler.get();
-            profilerFiller.push("spawnproofer\u001erender");
+        WorldRenderEvents.END_MAIN.register(context -> {
             OverlayManager.render(context);
-            profilerFiller.popPush("draw");
             OverlayManager.draw();
-            profilerFiller.pop();
-            profilerFiller.pop();
         });
     }
 }
